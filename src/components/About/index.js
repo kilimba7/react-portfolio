b04ce90f166882e802323f50e0def6648ebee7e5
Kilimba <dyaul003@umn.edu>
2022-09-18 15:46:04 -0500

    added About section

---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import coverImage from "../../assets/cover/avatar.jpg";
+
+function About() {
+    return(
+        <section>
+            <h1 id='about'>Kilimba Dyauli</h1>
+            <img src={coverImage} className="my-2" style={{ width: "10%" }} alt="cover" />
+            <div className='my-2'>
+                <p>
+                Welcome to my portfolio! I'm a junior dev, with a skill set in fullstack development. Some of my skills include 
+                  Test Driven Development and Object Oriented Programming. I've attached two of my projects below. Thank you!
+                </p>
+            </div>
+        </section>
+    );
+}
+
+
+export default About;
