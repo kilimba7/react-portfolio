823309c6da3727a304f8bde13c62ee758d5cd6b2
Kilimba <dyaul003@umn.edu>
2022-10-06 22:02:50 -0500

    added my new project

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -16,10 +16,10 @@ const Projects = ({ project }) => {
       url2: 'https://github.com/kilimba7/Movie-Finder'
     },
     {
-      name: 'Text-Editor',
+      name: 'Apex-Filter',
       project: 'Portfolio',
-      url1: 'https://vast-caverns-25905.herokuapp.com/',
-      url2: 'https://github.com/kilimba7/text-editor'
+      url1: 'https://gamer-6.herokuapp.com/',
+      url2: 'https://github.com/kilimba7/apex-filter'
 
     },
     {
@@ -28,25 +28,7 @@ const Projects = ({ project }) => {
       url1: 'https://community-recipe-calendar.herokuapp.com/',
       url2: 'https://github.com/kilimba7/Recipe-Me'
     },
-    {
-        name: 'Tech-Blog',
-        project: 'Portfolio',
-        url1: 'https://morning-coast-35493.herokuapp.com/',
-        url2: 'https://github.com/kilimba7/tech-blog'
-        
-    },
-    {
-        name: 'Note-Taker',
-        project: 'Portfolio',
-        url1: 'https://kilimba-note-taker.herokuapp.com/',
-        url2: 'https://github.com/kilimba7/note-taker'
-    },
-    {
-      name: 'Team-Profile',
-      project: 'Portfolio',
-      url1: 'https://drive.google.com/file/d/1FQz6FwwcrzI9mvRzEWKhW1vaBp68_C6M/view',
-      url2: 'https://github.com/kilimba7/profile-generator'
-    },
+   
   ]);
 
   const currentPhotos = photos.filter(photo => photo.project === project);
